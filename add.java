49330cb213e19601f33e66b48bfa6e10b3e7c029
Surender adepu <surenderadepu239@gmail.com>
2023-01-06 14:02:01 +0000

    modified java program

--- add.java
+++ add.java
@@ -2,8 +2,8 @@ public class SumOfNumbers1
 {  
 public static void main(String args[])   
 {  
-int n1 = 225, n2 = 115, sum;  
-sum = n1 + n2;  
+int n1 = 225, n2 = 115, n3 = 220, sum;  
+sum = n1 + n2 + n3;
 System.out.println("The sum of numbers is: "+sum);  
 }  
 }  
